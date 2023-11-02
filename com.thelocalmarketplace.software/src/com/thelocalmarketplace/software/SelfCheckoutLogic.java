738cfa6498319bc1cf882a253f6477c6c5d8c3cb
jaimie.marchuk <jaimie.marchuk@gmail.com>
2023-11-02 15:50:56 -0700

    Some planning for rest of software

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckoutLogic.java
@@ -0,0 +1,24 @@
+package com.thelocalmarketplace.software;
+
+public class SelfCheckoutLogic {
+	public SelfCheckoutLogic(){
+		// variable for total cost
+		// variable for current amount of money?
+		// variable for expected weight
+		// variable for actual weight
+		//
+
+		// make a coin controller
+		// add item controller
+		// scale controller?
+		//  ^^ is discrepancy alert apart of controller, maybe idk
+
+		// start session should wipe all values to start new session
+		// prolly implemented with button in mind
+
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
