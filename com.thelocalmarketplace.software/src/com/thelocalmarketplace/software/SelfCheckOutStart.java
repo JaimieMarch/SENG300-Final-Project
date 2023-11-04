8626e2ba222c4c97cbced9581c6248501d607f50
Kenny1534 <kennyzeng81@gmail.com>
2023-11-04 12:04:13 -0600

    Update SelfCheckOutStart.java
    Added a "OptionsText" method and implemented it

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckOutStart.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckOutStart.java
@@ -29,26 +29,31 @@ public class SelfCheckOutStart extends SelfCheckOutStationScanner {
    
     public class main{
     	public main(String[] args) {
+    	//initialization of 5 key components
     	SelfCheckoutStation scs = new SelfCheckoutStation();
     	ApplicationContext ac = new ApplicationContext(scs);
     	PowerGrid pg = PowerGrid.instance();
     	PayWithCoin pc = new PayWithCoin();
     	WeightDiscrepancy wd = new WeightDiscrepancy();
     	
-    	
+    	//Plugging in and then turning on this instance of the station
     	ac.plugIn(pg);
     	ac.turnOn();
     	
+    	//initializing the database for the program
     	DatabaseSeeder.seedDatabase();
     	
+    	//displays a start text and prompts user to hit enter before continuing
     	main.StartSession();
     	
-    	
+    	//displays the options available and stores the option given by user
+    	//into a string "select" that can be used to continue further.
+    	main.OptionText();
     	
   
     }
 
-    	public void Status() {
+		public void Status() {
         	public static boolean sessionStarted = false;
     	
         private static void StartSession() {
@@ -64,5 +69,17 @@ public class SelfCheckOutStart extends SelfCheckOutStationScanner {
         	scanner.close();
         	
         }
+    	private static void OptionText() {
+    		Scanner scanner = new Scanner(System.in);
+    		String select;
+    		System.out.println("Press 1 to add an item");
+    		System.out.println("Press 2 to remove an item");
+    		System.out.println("Press 3 to pay");
+    		System.out.println("Press 4 to add your own bag");
+    		System.out.println("Press 5 to select a language");
+    		System.out.println("Press 9 to request an attendant");
+    		select = scanner.nextLine();
+    		scanner.close();
+		}
     }
 }
