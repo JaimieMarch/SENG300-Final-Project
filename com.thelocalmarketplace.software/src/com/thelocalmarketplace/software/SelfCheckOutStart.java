cee04242c9a9586847296214dfb47ab09677fbb1
Kenny1534 <kennyzeng81@gmail.com>
2023-11-04 13:54:44 -0600

    Update SelfCheckOutStart.java
    Changed functionality of Status & removed OptionsText

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckOutStart.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckOutStart.java
@@ -27,14 +27,14 @@ public class SelfCheckOutStart extends SelfCheckOutStationScanner {
         return context;
     }
    
-    public class main{
-    	public main(String[] args) {
+    public void main(String[] args) {
     	//initialization of 5 key components
     	SelfCheckoutStation scs = new SelfCheckoutStation();
     	ApplicationContext ac = new ApplicationContext(scs);
     	PowerGrid pg = PowerGrid.instance();
-    	Payment pc = new Payment();
+    	PayWithCoin pc = new PayWithCoin();
     	WeightDiscrepancy wd = new WeightDiscrepancy();
+    	
     	
     	//Plugging in and then turning on this instance of the station
     	ac.plugIn(pg);
@@ -44,42 +44,23 @@ public class SelfCheckOutStart extends SelfCheckOutStationScanner {
     	DatabaseSeeder.seedDatabase();
     	
     	//displays a start text and prompts user to hit enter before continuing
-    	main.StartSession();
+    	Status();
     	
-    	//displays the options available and stores the option given by user
-    	//into a string "select" that can be used to continue further.
-    	main.OptionText();
-    	
-  
     }
 
-		public void Status() {
-        	public static boolean sessionStarted = false;
-    	
-        private static void StartSession() {
-        	Scanner scanner = new Scanner(System.in);
-        	if(sessionStarted == true) {
-        		return;
-        	}
-        	sessionStarted = true;
-        	System.out.println("Welcome to self checkout!");
-        	System.out.println("Enter 1 to begin");
-        	scanner.nextLine();
-        	System.out.println("Session Starting!");
-        	scanner.close();
-        	
-        }
-    	private static void OptionText() {
-    		Scanner scanner = new Scanner(System.in);
-    		String select;
-    		System.out.println("Press 1 to add an item");
-    		System.out.println("Press 2 to remove an item");
-    		System.out.println("Press 3 to pay");
-    		System.out.println("Press 4 to add your own bag");
-    		System.out.println("Press 5 to select a language");
-    		System.out.println("Press 9 to request an attendant");
-    		select = scanner.nextLine();
-    		scanner.close();
-		}
+	public void Status() {
+        boolean sessionStarted = false;
+
+        Scanner scanner = new Scanner(System.in);
+       	if(sessionStarted == true) {
+       		scanner.close();
+       		return;
+       	}
+       	sessionStarted = true;
+       	System.out.println("Welcome to self checkout!");
+       	System.out.println("Enter 1 to begin");
+       	scanner.nextLine();
+       	System.out.println("Session Starting!");
+       	scanner.close();
     }
 }
