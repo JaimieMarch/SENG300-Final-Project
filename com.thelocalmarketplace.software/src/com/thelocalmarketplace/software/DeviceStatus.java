76c468656c6895afd50a8fb3662aaec29c17b3da
jaimie.marchuk <jaimie.marchuk@gmail.com>
2023-11-04 21:42:00 -0700

    Adding more names to files

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/DeviceStatus.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/DeviceStatus.java
@@ -1,5 +1,12 @@
 package com.thelocalmarketplace.software;
 
+
+//        Ananya, Jain: 30196069
+//        Emily, Williams: 30122865
+//        Jaimie, Marchuk: 30112841
+//        Kenny, ZENG: 30151985
+//        Yang, YANG: 30156356
+
 public class DeviceStatus 
 {
 	public static final DeviceStatus UN_START = new DeviceStatus(0, "Function unstart");
